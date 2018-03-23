882282b48df486099ef15556c68acf970fb9af03
Miguel Rodriguez <miglos@hotmail.es>
2018-03-23 23:40:14 +0100

    First changes, mocking up

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,38 @@
-
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
-require('./bootstrap');
-
-window.Vue = require('vue');
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
-const app = new Vue({
-    el: '#app'
+var app = new Vue({
+	el: '#app',
+	data: {
+		title: sample.title,
+		address: sample.address,
+		about: sample.about,
+		headerImageStyle: {
+			'background-image': 'url(sample/header.jpg)'
+		},
+		amenities: sample.amenities,
+		prices: sample.prices,
+		contracted: true,
+		modalOpen: false
+	},
+	methods: {
+		escapeKeyListener: function(evt) {
+			if (evt.keyCode === 27 && this.modalOpen) {
+				this.modalOpen = false;
+			}
+		}
+	},
+	watch: {
+		modalOpen: function () {
+			var className = 'modal-open';
+			if (this.modalOpen) {
+				document.body.classList.add(className);
+			} else {
+				document.body.classList.remove(className);
+			}
+		}
+	},
+	created: function() {
+		document.addEventListener('keyup', this.escapeKeyListener);
+	},
+	destroyed: function() {
+		document.removeEventListener('keyup', this.escapeKeyListener);
+	}
 });
