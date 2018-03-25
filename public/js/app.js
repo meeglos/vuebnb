585a53cba38550738a100f1744243a8cfbf173e9
Miguel Rodriguez <miglos@hotmail.es>
2018-03-25 20:17:02 +0200

    ListingSummary component

--- public/js/app.js
+++ public/js/app.js
@@ -9567,8 +9567,6 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 
 
-// let model = JSON.parse(window.vuebnb_listing_model);
-// model = populateAmenitiesAndPrices(model);
 var serverData = JSON.parse(window.vuebnb_server_data);
 var model = Object(__WEBPACK_IMPORTED_MODULE_0__js_helpers__["b" /* populateAmenitiesAndPrices */])(serverData.listing);
 
@@ -13093,15 +13091,19 @@ if (inBrowser && window.Vue) {
 /***/ (function(module, exports, __webpack_require__) {
 
 var disposed = false
+function injectStyle (ssrContext) {
+  if (disposed) return
+  __webpack_require__(80)
+}
 var normalizeComponent = __webpack_require__(0)
 /* script */
-var __vue_script__ = __webpack_require__(80)
+var __vue_script__ = __webpack_require__(81)
 /* template */
-var __vue_template__ = __webpack_require__(81)
+var __vue_template__ = __webpack_require__(86)
 /* template functional */
 var __vue_template_functional__ = false
 /* styles */
-var __vue_styles__ = null
+var __vue_styles__ = injectStyle
 /* scopeId */
 var __vue_scopeId__ = null
 /* moduleIdentifier (server only) */
@@ -13137,14 +13139,30 @@ module.exports = Component.exports
 
 /***/ }),
 /* 80 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 81 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__js_helpers__ = __webpack_require__(16);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__ListingSummary_vue__ = __webpack_require__(82);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__ListingSummary_vue___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1__ListingSummary_vue__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+
 
 
 
@@ -13154,18 +13172,162 @@ var listing_groups = Object(__WEBPACK_IMPORTED_MODULE_0__js_helpers__["a" /* gro
 /* harmony default export */ __webpack_exports__["default"] = ({
   data: function data() {
     return { listing_groups: listing_groups };
+  },
+
+  components: {
+    ListingSummary: __WEBPACK_IMPORTED_MODULE_1__ListingSummary_vue___default.a
   }
 });
 
 /***/ }),
-/* 81 */
+/* 82 */
+/***/ (function(module, exports, __webpack_require__) {
+
+var disposed = false
+function injectStyle (ssrContext) {
+  if (disposed) return
+  __webpack_require__(83)
+}
+var normalizeComponent = __webpack_require__(0)
+/* script */
+var __vue_script__ = __webpack_require__(84)
+/* template */
+var __vue_template__ = __webpack_require__(85)
+/* template functional */
+var __vue_template_functional__ = false
+/* styles */
+var __vue_styles__ = injectStyle
+/* scopeId */
+var __vue_scopeId__ = null
+/* moduleIdentifier (server only) */
+var __vue_module_identifier__ = null
+var Component = normalizeComponent(
+  __vue_script__,
+  __vue_template__,
+  __vue_template_functional__,
+  __vue_styles__,
+  __vue_scopeId__,
+  __vue_module_identifier__
+)
+Component.options.__file = "resources\\assets\\components\\ListingSummary.vue"
+
+/* hot reload */
+if (false) {(function () {
+  var hotAPI = require("vue-hot-reload-api")
+  hotAPI.install(require("vue"), false)
+  if (!hotAPI.compatible) return
+  module.hot.accept()
+  if (!module.hot.data) {
+    hotAPI.createRecord("data-v-4f9cfef4", Component.options)
+  } else {
+    hotAPI.reload("data-v-4f9cfef4", Component.options)
+  }
+  module.hot.dispose(function (data) {
+    disposed = true
+  })
+})()}
+
+module.exports = Component.exports
+
+
+/***/ }),
+/* 83 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 84 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+/* harmony default export */ __webpack_exports__["default"] = ({
+  props: ['listing'],
+  computed: {
+    backgroundImageStyle: function backgroundImageStyle() {
+      return {
+        'background-image': 'url("' + this.listing.thumb + '")'
+      };
+    }
+  }
+});
+
+/***/ }),
+/* 85 */
 /***/ (function(module, exports, __webpack_require__) {
 
 var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("div", [_vm._v("Vuebnb home page")])
+  return _c("div", { staticClass: "listing-summary" }, [
+    _c("div", { staticClass: "wrapper" }, [
+      _c("div", { staticClass: "thumbnail", style: _vm.backgroundImageStyle }),
+      _vm._v(" "),
+      _c("div", { staticClass: "info title" }, [
+        _c("span", [_vm._v(_vm._s(_vm.listing.price_per_night))]),
+        _vm._v(" "),
+        _c("span", [_vm._v(_vm._s(_vm.listing.title))])
+      ]),
+      _vm._v(" "),
+      _c("div", { staticClass: "info address" }, [
+        _vm._v(_vm._s(_vm.listing.address))
+      ])
+    ])
+  ])
+}
+var staticRenderFns = []
+render._withStripped = true
+module.exports = { render: render, staticRenderFns: staticRenderFns }
+if (false) {
+  module.hot.accept()
+  if (module.hot.data) {
+    require("vue-hot-reload-api")      .rerender("data-v-4f9cfef4", module.exports)
+  }
+}
+
+/***/ }),
+/* 86 */
+/***/ (function(module, exports, __webpack_require__) {
+
+var render = function() {
+  var _vm = this
+  var _h = _vm.$createElement
+  var _c = _vm._self._c || _h
+  return _c(
+    "div",
+    { staticClass: "home-container" },
+    _vm._l(_vm.listing_groups, function(group, country) {
+      return _c("div", { staticClass: "listing-summary-group" }, [
+        _c("h1", [_vm._v("Places in " + _vm._s(country))]),
+        _vm._v(" "),
+        _c(
+          "div",
+          { staticClass: "listing-summaries" },
+          _vm._l(group, function(listing) {
+            return _c("listing-summary", {
+              key: listing.id,
+              attrs: { listing: listing }
+            })
+          })
+        )
+      ])
+    })
+  )
 }
 var staticRenderFns = []
 render._withStripped = true
